559ee67f9c2dccd0309545bded1a00d1f6c61de9
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2016-09-13 18:22:08 +0600

    Obj loader still in progress.

--- obj_reader.nim
+++ obj_reader.nim
@@ -1,9 +1,9 @@
-import tables
+import tables, strutils
 
 type
   Vert* = object
     x, y, z: float
-  
+
   Face* = array[0..2, int]
 
   Mesh* = object
@@ -14,12 +14,32 @@ type
     meshes: Table[string, Mesh]
 
 proc loadObj*(fileName: string): Model =
+  result.meshes = initTable[string, Mesh]()
+
   var f: File
   if open(f, fileName):
     try:
+      var currentMeshName = ""
       while not f.endOfFile:
         let line = f.readLine
-        echo line
+
+        let tokens = line.split(" ")
+
+        if tokens.len == 0:
+          continue
+
+        let command = tokens[0]
+
+        if tokens.len == 2 and command == "o":
+          let meshName = tokens[1]
+          currentMeshName = meshName
+          result.meshes[meshName] = Mesh(verts: @[], faces: @[])
+          continue
+
+        if currentMeshName != "" and tokens.len == 4 and command == "v":
+          echo currentMeshName & ": " & line
+          continue
+
     except IOError:
       echo "IO error!"
       raise
