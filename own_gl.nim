1e6e54c2e62122cbe4c434092bc270af5b4da934
Andrey Gladyshev <Andrey_Gladyshev@epam.com>
2016-09-13 14:35:13 +0600

    Sdl initialization added.

--- own_gl.nim
+++ own_gl.nim
@@ -5,4 +5,26 @@ var
   buf = render_buffer.init[RenderColor]()
 
 when isMainModule:
-  discard
+
+  discard sdl2.init(INIT_VIDEO)
+  let
+    window = createWindow("Own GL", 100, 100, 256, 256, SDL_WINDOW_SHOWN)
+    render = createRenderer(window, -1, Renderer_Software)
+
+  render.setDrawColor 0, 0, 0, 255
+  render.clear
+  #draw from render buffer here
+  render.present
+
+  var
+    done = false
+    evt = sdl2.defaultEvent
+
+  while not done:
+    while pollEvent(evt):
+      if evt.kind == QuitEvent:
+        done = true
+        break
+
+  destroy render
+  destroy window
