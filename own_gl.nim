8bd89b0b918ee612df3c9d71a32cd124363a609a
Andrey Gladyshev <vodomerkin@gmail.com>
2016-09-14 19:42:04 +0600

    Workaround to show some other models.

--- own_gl.nim
+++ own_gl.nim
@@ -5,18 +5,18 @@ import render_buffer, color, raster, types, geometry
 import algorithm, random, basic3d
 
 const
-  WINDOW_WIDTH = 256
-  WINDOW_HEIGHT = 256
+  WINDOW_WIDTH = 900
+  WINDOW_HEIGHT = 900
 
 var
   buf = render_buffer.init[RenderColor](WINDOW_WIDTH, WINDOW_HEIGHT)
 
-  model = loadObj("models/african_head/african_head.obj")
+  model = loadObj("models/Tails/Tails.obj")
 
-proc applyOffset(v: Point3d, offset: float = 1.0): Vec2i =
+proc applyOffset(v: Point3d, offset: float = 1.0, y = 0): Vec2i =
   let offsetDouble = offset * 2.0
   result.x = int((v.x + offset) * WINDOW_WIDTH / offsetDouble)
-  result.y = int((v.y + offset) * WINDOW_HEIGHT / offsetDouble) 
+  result.y = int((v.y + offset) * WINDOW_HEIGHT / offsetDouble) - y 
 
 proc drawRenderBufferToWindow(buf: RenderBuffer[RenderColor], render: RendererPtr) =
   var pointOperations = 0
@@ -47,9 +47,9 @@ when isMainModule:
 
   for tri in model.triangles():
     var vs: array[3, Vec2i]
-    vs[0] = applyOffset(tri[0], 1.0)
-    vs[1] = applyOffset(tri[1], 1.0)
-    vs[2] = applyOffset(tri[2], 1.0)
+    vs[0] = applyOffset(tri[0], 5.0, WINDOW_HEIGHT div 2 - 10)
+    vs[1] = applyOffset(tri[1], 5.0, WINDOW_HEIGHT div 2 - 10)
+    vs[2] = applyOffset(tri[2], 5.0, WINDOW_HEIGHT div 2 - 10)
 
     let
       intensity = dot(tri.normal(), lightDir)
