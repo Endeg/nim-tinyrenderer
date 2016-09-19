14655cb2f7eae2ca5b864cc819eae19a3d012ab8
Andrey Gladyshev <vodomerkin@gmail.com>
2016-09-19 21:05:46 +0600

    Using seq instead of table in RenderBuffer.

--- render_buffer.nim
+++ render_buffer.nim
@@ -3,20 +3,16 @@ import tables, color
 #TODO: better name?
 
 type
-  RenderBufferKey = tuple
-    x: int
-    y: int
-
   RenderBuffer*[P] = object
     #TODO: Use seq[x + y * w] instead
-    map: Table[RenderBufferKey, P]
-    defaultValue: P
+    data: seq[P]
     width*: int
     height*: int
 
 proc init*[P](defaultValue: P, width: int = 0, height: int = 0): RenderBuffer[P] =
-  result.map = initTable[RenderBufferKey, P]()
-  result.defaultValue = defaultValue
+  result.data = newSeq[P](width * height)
+  for i in result.data.low..result.data.high:
+    result.data[i] = defaultValue
   result.width = width
   result.height = height
 
@@ -24,13 +20,23 @@ proc set*[P](self: var RenderBuffer[P], x: int, y: int, value: P) =
   if self.width > 0 and self.height > 0 and 
       (x < 0 or x >= self.width or y < 0 or y >= self.height):
     return
-  self.map[(x, y)] = value
+  self.data[x + y * self.width] = value
 
 iterator entries*[P](self: RenderBuffer[P]): (int, int, P) =
-  for k, v in self.map.pairs():
-    yield (k.x, k.y, v)
+  for x in 0..self.width - 1:
+    for y in 0..self.height - 1:
+      yield (x, y, self.data[x + y * self.width])
 
 proc get*[P](self: RenderBuffer[P], x, y: int): P =
-  result = self.defaultValue
-  if self.map.hasKey((x, y)):
-    result = self.map[(x, y)]
+  var
+    xx = x
+    yy = y
+  if xx > self.width - 1:
+    xx = self.width - 1
+  if yy > self.height - 1:
+    yy = self.height - 1
+  if xx < 0:
+    xx = 0
+  if yy < 0:
+    yy = 0
+  result = self.data[xx + yy * self.width]
